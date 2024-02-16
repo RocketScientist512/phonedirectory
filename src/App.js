945ba1334f9cee8bfb3e14742bc0cd5cc332994d
Rohan Shorey <rohanshorey@WINDELLAP-734.local>
2024-02-16 12:04:34 +0530

    updates from yesterday

--- src/App.js
+++ src/App.js
@@ -4,20 +4,46 @@ import './App.css';
 
 class App extends Component {
 
-  
+  clickHandler(message){
+    alert(message);
+  }
+
+  constructor(){
+    super();
+    this.state={
+      subscribersListToShow: []
+    }
+    console.log("Constructor Called");
+  }
+
+  // componentDidMount(){
+  //   console.log("Component Did Mount Called");
+  //   let newSubscriber ={
+  //     id: 1,
+  //     name: 'Rohan Shorey',
+  //     phone: '9910430954'
+  //   }
+  // let subscribersList = this.state.subscribersListToShow;
+  // subscribersList.push(newSubscriber);
+
+  // this.setState({subscribersListToShow: subscribersList})  ;
+  // }
+
+
   render() {
-    let subscribers = [
-      {
-        id: 1,
-        name: 'Rohan Shorey',
-        phone: '9090909090'
-      },
-      {
-        id: 2,
-        name: 'Pranav Shorey',
-        phone:'8989898989'
-      }
-    ]
+    console.log("Render Called");
+    // let subscribers = [
+    //   {
+    //     id: 1,
+    //     name: 'Rohan Shorey',
+    //     phone: '9090909090'
+    //   },
+    //   {
+    //     id: 2,
+    //     name: 'Pranav Shorey',
+    //     phone:'8989898989'
+    //   }
+    // ]
     return (
       <div>
         {/* <Header/>
@@ -37,11 +63,11 @@ class App extends Component {
               </div>
 
               {
-                subscribers.map(sub =>{
+                this.state.subscribersListToShow.map(sub =>{
                   return <div key={sub.id} className='grid-container'>
                     <span className='grid-item'>{sub.name}</span>
                     <span className='grid-item'>{sub.phone}</span>
-                    <span className='delete-item'>Delete</span>
+                    <button className='delete-item' onClick={this.clickHandler.bind(this, "Delete Clicked")}>Delete</button>
                     </div>
                 })
               }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AddSubscriber from './AddSubscriber';
 import reportWebVitals from './reportWebVitals';
+import './common/common.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> 
+    {/* <AddSubscriber/> */}
   </React.StrictMode>
 );
+
+// ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
